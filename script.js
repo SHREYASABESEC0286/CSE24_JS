06a1ed56dfb43e0929c884aa8c25ff6f0a7b9062
SHREYASABESE0286 <jaiswalshreyas169@gmail.com>
2026-02-24 15:45:42 +0530

    initail commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log("hellow World");
+// document.write("hellow World");
+var num = null;
+console.log(num);
+console.log(typeof(num));
+// num = false;
+//javaScript operators
+// addition subtraction multiplication division modulus
+// increment ++
+
