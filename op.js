2534894461eaa372f9303c7bf4ee844ce6a6c411
SHREYASABESE0286 <jaiswalshreyas169@gmail.com>
2026-02-24 16:19:53 +0530

    added files and editing done

---
+++ op.js
@@ -0,0 +1,4 @@
+console.log('a-b=');
+reult=a-b;
+console.log(result);
+
--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// console.log("hellow World");
-// document.write("hellow World");
-var num = null;
-console.log(num);
-console.log(typeof(num));
+// console.log("hello World");
+// document.write("hello World");
+// var num = null;
+// console.log(num);
+// console.log(typeof(num));
 // num = false;
 //javaScript operators
 // addition subtraction multiplication division modulus
 // increment ++
-
+var count = 0;
+console.log("starting loop");
+for (count;count<10;count++){
+    console.log("current count is " +count);
+}
